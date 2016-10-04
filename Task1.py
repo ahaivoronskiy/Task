a584dc19ab104d2ffef7f2bc745d3a8b4bdbe1e9
DESKTOP-D2CCH6T\pervolo <ahaivoronsliy@pervolo.com>
2016-10-04 15:40:00 +0300

    DataBase2

--- Task1.py
+++ Task1.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
-
 from wtforms import Form, StringField, validators
+from flask_sqlalchemy import SQLAlchemy
 
 class DemoForm(Form):
     test_name = StringField("Test", [validators.Length(min=4, max=128, message= 'No correct name')])
@@ -21,3 +21,28 @@ def index():
 
 if __name__ == '__main__':
     app.run(port=8888, debug=True)
+
+# DataBase
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\path\\to\\database.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement= True )
+    name = db.Column(db.String(128))
+    email = db.Column(db.String(256))
+    comment = db.Column (db.String)
+    phone = db.Column(db.String(128))
+
+    def __init__(self, name, email, comment, phone):
+        self.email = email
+        self.name = name
+        self.comment = comment
+        self.phone = phone
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+db.create_all()
+admin = User('admin', 'admin@example.com')
+db.session.add(admin)
+db.session.commit()
+
