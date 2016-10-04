ab18db606718a877a3a5a6325706df6793366c10
DESKTOP-D2CCH6T\pervolo <ahaivoronsliy@pervolo.com>
2016-10-04 11:13:16 +0300

    form2

--- Task1.py
+++ Task1.py
@@ -5,21 +5,14 @@ from wtforms import Form, StringField, validators
 class DemoForm(Form):
     test_name = StringField("Test", [validators.Length(min=4, max=128, message= 'No correct name')])
     test_email = StringField('Email',[validators.Email (message='No correct E-mail')] )
-    test_comment = StringField ('Comment')
-
+    test_comment = StringField ("Comment", [validators.Length(min=4,  message= 'Enter your comment')])
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    name = DemoForm()
-    email = DemoForm()
-    comment = DemoForm()
-    return render_template("index.html", name=name, email=email, comment=comment)
-
-
-
-
+    form = DemoForm()
+    return render_template("index.html", form=form )
 if __name__ == '__main__':
     app.run(port=8888, debug=True)
