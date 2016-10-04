8a277050e1fde9ba4d26069c8c426e3b3b813bd4
DESKTOP-D2CCH6T\pervolo <ahaivoronsliy@pervolo.com>
2016-10-04 14:51:14 +0300

    DataBase1

--- Task1.py
+++ Task1.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from wtforms import Form, StringField, validators
 
@@ -6,13 +6,18 @@ class DemoForm(Form):
     test_name = StringField("Test", [validators.Length(min=4, max=128, message= 'No correct name')])
     test_email = StringField('Email',[validators.Email (message='No correct E-mail')] )
     test_comment = StringField ("Comment", [validators.Length(min=4,  message= 'Enter your comment')])
+    test_phone = StringField ('Phone number')
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    form = DemoForm()
+    form = DemoForm(request.form)
+
+    if request.method == 'POST' and form.validate():
+        return "All data in form is valid"
+
     return render_template("index.html", form=form )
+
 if __name__ == '__main__':
     app.run(port=8888, debug=True)
